6827cfaf9a340f4264c30603d6d999a4c9672b16
cindy.yeh <fonta9996@gmail.com>
2023-10-23 15:02:23 +0800

    [初始][CSROP-3956] 專案初始

---
+++ .vscode/blueprint-templates/vue-component/__pascalCase_name__.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="{{ pascalCase name }}">
+    {{ pascalCase name }}
+  </div>
+</template>
+
+<script setup>
+// const props = defineProps({})
+// const emit = defineEmits([])
+</script>
+
+<style lang="scss">
+.{{ pascalCase name }} {
+}
+
+// xl | lg | md | sm
+// @screen xl {}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <TheWelcome msg="104 前端樣板"></TheWelcome>
+  </main>
+</template>
