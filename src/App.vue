8bf6be218cb6b2681a6ca9b03fe4a729ad4df7ad
cindy.yeh <fonta9996@gmail.com>
2023-10-24 14:52:56 +0800

    [初始][CSROP-3956] 初始設定 & 移除範例檔案

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,15 @@
 <script setup>
 import { useHead } from '@vueuse/head'
 
-useHead({
-  title: '104z-temp 前端專案共用模板',
-  meta: [
-    {
-      name: 'description',
-      content: '104z-temp 前端專案共用模板 vue3+pinia+vite',
-    },
-  ],
-})
+useHead()
 </script>
 
 <template>
   <section>
-    <header>
-      <img
-        alt="104 logo"
-        src="@/assets/images/logo.svg"
-        width="200"
-        height="150"
-      >
-    </header>
-
     <RouterView></RouterView>
   </section>
 </template>
 
 <style lang="scss">
 @use '@/assets/styles/base.scss';
-
-#app {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100%;
-}
 </style>
---
+++ src/views/Index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Index">
+    Index
+  </div>
+</template>
+
+<script setup>
+// const props = defineProps({})
+// const emit = defineEmits([])
+</script>
+
+<style lang="scss">
+.Index {
+}
+
+// xl | lg | md | sm
+// @screen xl {}
+</style>
