3b307a6c6c3e01f78271d55f7096cac83f63a8ca
BeanBeam11 <linda32go1819@gmail.com>
2023-10-31 10:24:32 +0800

    [功能][CSROP-3981] 工具 store

--- src/common/domainConfig.js
+++ src/common/domainConfig.js
@@ -2,4 +2,6 @@ export const mainUrl = `https://www.${ import.meta.env.VITE_MAIN_DOMAIN }/`
 export const blogUrl = `https://blog.${ import.meta.env.VITE_MAIN_DOMAIN }/`
 export const profileUrl = `https://profile.${ import.meta.env.VITE_MAIN_DOMAIN }/`
 export const giverUrl = `https://giver.${ import.meta.env.VITE_MAIN_DOMAIN }/`
+export const guideUrl = import.meta.env.VITE_APP_GUIDE_URL
+export const nabiUrl = import.meta.env.VITE_APP_NABI_URL
 export const beCollegeUrl = `https://be.college.${ import.meta.env.VITE_MAIN_DOMAIN }/`
