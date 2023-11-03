1ec0411b763d6de89ce65f15a934537423b12aca
cindy.yeh <fonta9996@gmail.com>
2023-11-03 16:51:41 +0800

    [功能][CSROP-4050] GlobalFooter 模組

---
+++ src/components/GlobalFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="GlobalFooter mt-auto">
+    <div class="text-12 leading-20 px-16 py-12 h-44 text-center bg-black text-white md:hidden">
+      一零四資訊科技股份有限公司 版權所有 ©{{ new Date().getFullYear() }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+#app-footer {
+  @apply hidden md:block;
+}
+
+.GlobalFooter {
+}
+</style>
