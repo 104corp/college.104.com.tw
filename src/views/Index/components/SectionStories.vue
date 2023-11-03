6b517966cb56360bd1e306903a592d5083475f23
BeanBeam11 <linda32go1819@gmail.com>
2023-11-03 13:18:16 +0800

    [功能][CSROP-3965] 調整卡片架構及變數

--- src/views/Index/components/SectionStories.vue
+++ src/views/Index/components/SectionStories.vue
@@ -13,46 +13,52 @@
       ></div>
       <div v-else>
         <div class="overflow-x-hidden mt-24 md:(overflow-hidden relative flex items-center justify-start)">
-          <div
+          <ul
+            ref="$slider"
             class="flex float-left transform transition-transform duration-500"
             :style="{ 'transform': `translateX(${positionX}px)` }"
+            @transitionend="resume"
           >
-            <a
+            <li
               v-for="profilePortfolio in profilePortfolioStore.list"
               :key="profilePortfolio.author"
-              :href="addQuery(profilePortfolio.url, utm)"
-              target="_blank"
-              class="group flex-shrink-0 mx-12 w-240 h-296 md:(w-400 h-402)"
+              class="flex-shrink-0 box-content px-12 w-240 h-296 md:(w-400 h-402)"
             >
-              <div class="relative flex flex-col items-center pb-20">
-                <div class="overflow-hidden w-full aspect-ratio-4/3 border border-gray-200 rounded-8">
-                  <BaseImage
-                    :src="profilePortfolio.cover"
-                    :alt="profilePortfolio.title"
-                    draggable="false"
-                    class="object-cover w-full h-full"
-                  ></BaseImage>
+              <a
+                :href="addQuery(profilePortfolio.url, utm)"
+                target="_blank"
+                class="group"
+              >
+                <div class="relative flex flex-col items-center pb-20">
+                  <div class="overflow-hidden w-full aspect-ratio-4/3 border border-gray-200 rounded-8">
+                    <BaseImage
+                      :src="profilePortfolio.cover"
+                      :alt="profilePortfolio.title"
+                      draggable="false"
+                      class="object-cover w-full h-full"
+                    ></BaseImage>
+                  </div>
+                  <div class="absolute bottom-0 overflow-hidden w-40 aspect-ratio-1 rounded-1/2 border border-white">
+                    <BaseImage
+                      :src="profilePortfolio.author.avatar"
+                      :alt="profilePortfolio.author.name"
+                      draggable="false"
+                      class="object-cover w-full h-full"
+                    ></BaseImage>
+                  </div>
                 </div>
-                <div class="absolute bottom-0 overflow-hidden w-40 aspect-ratio-1 rounded-1/2 border border-white">
-                  <BaseImage
-                    :src="profilePortfolio.author.avatar"
-                    :alt="profilePortfolio.author.name"
-                    draggable="false"
-                    class="object-cover w-full h-full"
-                  ></BaseImage>
+                <div class="font-700 text-center">
+                  <div class="text-(base gray-500) line-clamp-1">
+                    {{ profilePortfolio.author.name }}・{{ profilePortfolio.author.jobTitle }}
+                  </div>
+                  <div class="mx-auto my-8 w-20 border-(b-2 lake-200)"></div>
+                  <div class="text-18 md:group-hover:text-lake-200 line-clamp-1">
+                    {{ profilePortfolio.title }}
+                  </div>
                 </div>
-              </div>
-              <div class="font-700 text-center">
-                <div class="text-(base gray-500) line-clamp-1">
-                  {{ profilePortfolio.author.name }}・{{ profilePortfolio.author.jobTitle }}
-                </div>
-                <div class="mx-auto my-8 w-20 border-(b-2 lake-200)"></div>
-                <div class="text-18 md:group-hover:text-lake-200 line-clamp-1">
-                  {{ profilePortfolio.title }}
-                </div>
-              </div>
-            </a>
-          </div>
+              </a>
+            </li>
+          </ul>
           <div class="hidden layout-container md:(block absolute left-1/2 transform -translate-x-1/2 w-full) xl:max-w-1200">
             <div class="flex justify-between">
               <button
@@ -79,6 +85,7 @@
               'w-24 bg-(lake-200 op-100)': pageIndex === page,
               'bg-(gray-400 op-30)': pageIndex !== page,
             }"
+            @click="move(pageIndex)"
           ></button>
         </div>
       </div>
@@ -97,7 +104,9 @@
 import {
   ref, onMounted, computed
 } from 'vue'
-import { useWindowSize } from '@vueuse/core'
+import {
+  useWindowSize, useElementSize, useIntervalFn
+} from '@vueuse/core'
 import { addQuery } from '@/utils/urlHandler.js'
 import { useProfilePortfolio } from '@/stores/ProfilePortfolio.js'
 
@@ -106,6 +115,10 @@ const { width: windowWidth } = useWindowSize()
 
 const loading = ref(false)
 const index = ref(0)
+
+const $slider = ref(null)
+const { width: slideWidth } = useElementSize($slider)
+const { width: bodyWidth } = useElementSize(document.body)
 
 const utm = {
   utm_source: 'cweb_studentmainpage',
@@ -116,41 +129,48 @@ const moreUtm = {
   utm_medium: 'university_toolbox_seemore'
 }
 
-const pageSetCount = computed(() => {
-  return windowWidth.value > 1024 ? 3 : 1
+const startIndex = computed(() => {
+  return windowWidth.value > 1024 ? 1 : 0
 })
 
-const totalPage = computed(() => {
-  return Math.ceil(profilePortfolioStore.list.length / pageSetCount.value)
+const pageSetCount = computed(() => {
+  return windowWidth.value > 1024 ? 3 : 1
 })
 
 const page = computed(() => {
   return Math.floor(index.value / pageSetCount.value)
 })
 
+const totalPage = computed(() => {
+  return Math.ceil(profilePortfolioStore.list.length / pageSetCount.value)
+})
+
 const positionX = computed(() => {
-  const slideWidth = windowWidth.value > 1024 ? 424 : 264
-  const bodyWidth = document.body.clientWidth
-  return -(index.value + 0.5) * slideWidth + bodyWidth / 2
+  const listLength = profilePortfolioStore.list.length
+  return -(index.value + 0.5) / listLength * slideWidth.value + bodyWidth.value / 2
 })
 
 function move (toPage) {
+  pause()
+  
   if (toPage < 0) {
-    index.value = 1 + (totalPage.value - 1) * pageSetCount.value
+    index.value = startIndex.value + (totalPage.value - 1) * pageSetCount.value
   } else if (toPage > totalPage.value - 1) {
-    index.value = windowWidth.value > 1024 ? 1 : 0
+    index.value = startIndex.value
   } else {
-    const offsetNum = windowWidth.value > 1024 ? 1 : 0
-    index.value = offsetNum + toPage * pageSetCount.value
+    index.value = startIndex.value + toPage * pageSetCount.value
   }
 }
 
+const {
+  pause, resume 
+} = useIntervalFn(() => {
+  move(page.value + 1)
+}, 3000)
+
 onMounted(async () => {
   await profilePortfolioStore.getList()
-  index.value = windowWidth.value > 1024 ? 1 : 0
-  setInterval(() => {
-    move(page.value + 1)
-  }, 3000)
+  index.value = startIndex.value
 })
 </script>
 
