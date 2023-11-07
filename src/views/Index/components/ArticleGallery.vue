8d68ff747ef5e2e7d0d61d25a253cd1f2e6d70b7
BeanBeam11 <linda32go1819@gmail.com>
2023-11-07 17:10:54 +0800

    [功能][CSROP-4009] 變版樣式改為屬性寫法

--- src/views/Index/components/ArticleGallery.vue
+++ src/views/Index/components/ArticleGallery.vue
@@ -1,20 +1,23 @@
 <template>
   <div
     v-if="props.list.slice(0, 1).length"
-    class="ArticleGallery flex flex-col mt-8 md:(flex-row justify-center gap-48 mt-0)"
+    class="ArticleGallery flex flex-col mt-8"
+    md="flex-row justify-center gap-48 mt-0"
   >
     <a
       v-for="article in props.list.slice(0, 1)"
       :key="article.title"
       :href="article.link"
       target="_blank"
-      class="group flex flex-col gap-12 pb-12 border-(b gray-300) md:(w-420 border-none)"
+      class="group flex flex-col gap-12 pb-12 border-(b gray-300)"
+      md="w-420 border-none"
     >
       <div class="flex overflow-hidden border border-gray-200 rounded-16">
         <BaseImage
           :src="article.image"
           :alt="article.title"
-          class="w-full object-cover rounded-11 aspect-16/9 md:(transition-transform duration-300 linear) md:group-hover:(transform scale-110)"
+          class="w-full object-cover rounded-11 aspect-16/9"
+          md="transition-transform duration-300 linear group-hover:(transform scale-110)"
         ></BaseImage>
       </div>
       <div class="min-w-0">
@@ -25,17 +28,24 @@
           ></div>
           <div>{{ props.categoryName }}</div>
         </div>
-        <p class="text-16 leading-24 font-700 line-clamp-2 md:(text-18 transition-color duration-300 linear) md:group-hover:(text-lake-200)">
+        <p
+          class="text-16 leading-24 font-700 line-clamp-2"
+          md="text-18 transition-color duration-300 linear group-hover:(text-lake-200)"
+        >
           {{ article.title }}
         </p>
-        <p class="mt-4 text-14 line-clamp-2 md:(text-base line-clamp-3)">
+        <p
+          class="mt-4 text-14 line-clamp-2"
+          md="text-base line-clamp-3"
+        >
           {{ article.content }}
         </p>
       </div>
     </a>
     <div
       v-if="props.list.slice(1).length"
-      class="flex-1 md:w-1/2"
+      class="flex-1"
+      md="w-1/2"
     >
       <a
         v-for="article in props.list.slice(1)"
@@ -44,25 +54,38 @@
         target="_blank"
         class="group flex flex-row-reverse items-center justify-between gap-20 py-12 border-(b gray-300)"
       >
-        <div class="flex flex-shrink-0 overflow-hidden w-144 border border-gray-200 rounded-11 md:(w-200 rounded-16)">
+        <div
+          class="flex flex-shrink-0 overflow-hidden w-144 border border-gray-200 rounded-11"
+          md="w-200 rounded-16"
+        >
           <BaseImage
             :src="article.image"
             :alt="article.title"
-            class="w-full object-cover aspect-16/9 md:(transition-transform duration-300 linear) md:group-hover:(transform scale-110)"
+            class="w-full object-cover aspect-16/9"
+            md="transition-transform duration-300 linear group-hover:(transform scale-110)"
           ></BaseImage>
         </div>
         <div class="min-w-0">
-          <div class="flex items-center gap-8 text-(14 lake-200) font-700 md:(text-base)">
+          <div
+            class="flex items-center gap-8 text-(14 lake-200) font-700"
+            md="text-base"
+          >
             <div
               class="i-icon:pinned text-(16 lake-200)"
               :class="{ 'hidden': !article.isPinned }"
             ></div>
             <div>{{ props.categoryName }}</div>
           </div>
-          <p class="text-16 leading-24 font-700 line-clamp-2 md:(text-18 transition-color duration-300 linear) md:group-hover:(text-lake-200)">
+          <p
+            class="text-16 leading-24 font-700 line-clamp-2"
+            md="text-18 transition-color duration-300 linear group-hover:(text-lake-200)"
+          >
             {{ article.title }}
           </p>
-          <p class="mt-4 text-14 line-clamp-1 md:(text-base)">
+          <p
+            class="mt-4 text-14 line-clamp-1"
+            md="text-base"
+          >
             {{ article.content }}
           </p>
         </div>
--- src/views/Index/components/SectionArticle.vue
+++ src/views/Index/components/SectionArticle.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="SectionArticle py-40 bg-white md:pb-120">
+  <div
+    class="SectionArticle py-40 bg-white"
+    md="pb-120"
+  >
     <div class="text-(36 lake-200 center) leading-44 family-Bungee">
       NEWS
     </div>
     <h2 class="text-(20 gray-600 center) font-700">
       產業新訊
     </h2>
-    <div class="layout-container relative mx-auto mt-20 px-16 md:(mt-32 px-0)">
+    <div
+      class="layout-container relative mx-auto mt-20 px-16"
+      md="mt-32 px-0"
+    >
       <div
         v-if="loading"
-        class="loading w-full h-860 md:h-415"
+        class="loading w-full h-860"
+        md="h-415"
       ></div>
       <ArticleGallery
         v-else
@@ -17,11 +24,15 @@
         category-name="專題特輯"
       ></ArticleGallery>
     </div>
-    <div class="flex justify-center mt-24 md:mt-32">
+    <div
+      class="flex justify-center mt-24"
+      md="mt-32"
+    >
       <a
         :href="addQuery(storeArticleNews.moreLink, moreUtm)"
         target="_blank"
-        class="inline-block px-16 text-(base lake-200) font-700 border border-lake-200 rounded-4 md:px-32"
+        class="inline-block px-16 text-(base lake-200) font-700 border border-lake-200 rounded-4"
+        md="px-32"
       >更多產業新訊</a>
     </div>
   </div>
