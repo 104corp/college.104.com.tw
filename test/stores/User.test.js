6fa72c02d5ebcf43931cd2971d22c4df09ad8c20
cindy.yeh <fonta9996@gmail.com>
2023-11-07 16:22:59 +0800

    [功能][CSROP-3979] store User

--- test/stores/User.test.js
+++ test/stores/User.test.js
@@ -1,10 +1,12 @@
 import { createTestingPinia } from '@pinia/testing'
 import { useUser } from '@/stores/User.js'
 import defaultAvatar from '@/assets/img/headDef.png'
+import apiService from '@/apis/index'
+import { expect } from 'vitest'
 
-const pinia = createTestingPinia()
+const pinia = createTestingPinia({ stubActions: false })
 const store = useUser('User', pinia)
-describe('useUser', () => {
+describe('useUser-computed', () => {
   it('如果 profile 為 null 或沒有 avatar，avatar 應該返回默認的頭像', () => {
     expect(store.avatar).toBe(defaultAvatar)
     store.profile = {
@@ -29,5 +31,15 @@ describe('useUser', () => {
   it('如果 profile 是 null，isLogin 應該返回 false', () => {
     store.profile = null
     expect(store.isLogin).toBe(false)
+  })
+})
+describe('useUser-function', () => {
+  it('getProfile：執行api Service 並更新 store.profile', async () => {
+    store.profile = undefined
+    const spyService = vi.spyOn(apiService, 'getProfile').mockResolvedValue({ name: 'Alice Chang' })
+    await store.getProfile()
+
+    expect(store.profile).toEqual({ name: 'Alice Chang' })
+    expect(spyService).toHaveBeenCalled()
   })
 })
