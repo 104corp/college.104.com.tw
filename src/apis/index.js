6286fc493bf03939758684f7ad9032bdef88d622
cindy.yeh <fonta9996@gmail.com>
2023-11-08 11:36:40 +0800

    [功能][CSROP-4048] store resume  api

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import Auth_GetStatusInfo from './auth/getStatusInfo.js'
-
+import Home_ResumeSample from './home/ResumeSample.js'
 export default {
-  getProfile: Auth_GetStatusInfo.get
+  getProfile: Auth_GetStatusInfo.get,
+  getResumeSample: Home_ResumeSample.get
 }
