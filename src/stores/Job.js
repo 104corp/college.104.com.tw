7b21a30aa2d0cf2ffe84eb96978d86f828321abc
cindy.yeh <fonta9996@gmail.com>
2023-11-13 17:45:49 +0800

    [功能][CSROP] store Job api串接

--- src/stores/Job.js
+++ src/stores/Job.js
@@ -3,6 +3,7 @@ import {
   ref, computed 
 } from 'vue'
 import { mainUrl } from '@/common/domainConfig'
+import apiService from '@/apis/index'
 
 export const useJob = (id = 'Job', pinia) => {
   const _typeMap = ref({
@@ -33,82 +34,49 @@ export const useJob = (id = 'Job', pinia) => {
     }
   }))
 
+  const loading = {
+    getAllJobs: ref(false),
+    getTags: ref(false),
+    getAdvertises: ref(false)
+  }
+
   const getAllJobs = async () => {
-    const result = [
-      {
-        title: 'Software Engineer',
-        salary: '120k',
-        area: 'San Francisco',
-        link: 'https://example.com/job1'
-      },
-      {
-        title: 'Data Analyst',
-        salary: '80k',
-        area: 'New York',
-        link: 'https://example.com/job2'
-      },
-      {
-        title: '[日商] 咖樂迪咖啡農場 京站 小碧潭店 門市兼職人員 _時薪176元～180元(歡迎學生打工、二度就業!!)強力招募中!!',
-        salary: '時薪176~180元',
-        area: '新北市新店區',
-        link: 'https://example.com/job2'
-      },
-      {
-        title: '網頁設計師',
-        salary: '月薪 40,000 至 60,000 元',
-        area: '台北市信義區',
-        link: 'https://example.com/job8'
+    loading.getAllJobs.value = true
+    try {
+      const resultPt = await apiService.getPtJob()
+      const resultIntern = await apiService.getInternJob()
+      _typeMap.value.parttime.jobs = resultPt
+      _typeMap.value.intern.jobs = resultIntern
+
+      return {
+        parttime: resultPt,
+        intern: resultIntern
       }
-    ]
-
-    _typeMap.value.parttime.jobs = result
-    _typeMap.value.intern.jobs = result
-
-    return result
+    } finally {
+      loading.getAllJobs.value = false
+    }
   }
 
   const getAdvertises = async () => {
-    const result = [
-      {
-        image: 'https://picsum.photos/104/100',
-        link: 'https://example.com/link1'
-      },
-      {
-        image: 'https://picsum.photos/104/200',
-        link: 'https://example.com/link2'
-      },
-      {
-        image: 'https://picsum.photos/104/300',
-        link: 'https://example.com/link3'
-      },
-      {
-        image: 'https://picsum.photos/104/400',
-        link: 'https://example.com/link4'
-      }
-    ]
-
-    _advertises.value = result
-
-    return result
+    loading.getAdvertises.value = true
+    try {
+      const result = await apiService.getJobAdvertise()
+      _advertises.value = result
+      return result
+    } finally {
+      loading.getAdvertises.value = false
+    }
   }
 
   const getTags = async () => {
-    const result = [
-      {
-        name: 'tag1',
-        url: 'https://example.com/tag1',
-        type: 'parttime'
-      },
-      {
-        name: 'tag2',
-        url: 'https://example.com/tag2',
-        type: 'intern'
-      }
-    ]
-
-    _tags.value = result
-
-    return result
+    loading.getTags.value = true
+    try {
+      const result = await apiService.getJobTags()
+      _tags.value = result
+      return result
+    } finally {
+      loading.getTags.value = false
+    }
   }
 
   return defineStore(id, () => {
@@ -119,6 +87,7 @@ export const useJob = (id = 'Job', pinia) => {
       listedType,
       typeJobs,
       typeLink,
+      loading,
       getAllJobs,
       getAdvertises,
       getTags
