bef947e5829f60dfe4f9c4b24b3f6ad38760a5a0
cindy.yeh <fonta9996@gmail.com>
2023-11-22 11:59:27 +0800

    [功能][CSROP-4071] goTop icon更新及位置調整

--- src/components/GoTop.vue
+++ src/components/GoTop.vue
@@ -1,7 +1,8 @@
 <template>
   <button
     v-show="y > visibleAtY"
-    class="GoTop group w-56 h-56 rounded-1/2 shadow-button-gray bg-white fixed flex items-center justify-center z-9 bottom-93 right-12 md:(w-72 h-72 bottom-70 rounded-4) print:hidden"
+    class="GoTop group w-56 h-56 rounded-1/2 shadow-button-gray bg-white fixed flex items-center justify-center z-9 bottom-36 right-12 print:hidden"
+    md="w-72 h-72 bottom-70 rounded-4"
     @click="goTop()"
   >
     <i 
